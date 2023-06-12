feaae2b17154e112ccafb696cffd8d658fde61af
JC2197 <JoeConti21@gmail.com>
2023-06-12 12:20:56 -0400

    fixed sword

--- Assets/Scripts/weaponScript.cs
+++ Assets/Scripts/weaponScript.cs
@@ -16,13 +16,13 @@ public class weaponScript : MonoBehaviour
         if(player.direction.y > 0){ //north..
             if(player.direction.x < 0){ //west
                 player.currDirection = "NORTHWEST";
-                transform.localPosition = new Vector3(-0.0209999997f,0.200000003f,0.5f);
+                transform.localPosition = new Vector3(-0.0209999997f,0.133f,0.5f);
                 transform.localRotation = Quaternion.Euler(0,0,21.1808357f);
                 transform.localScale = new Vector3(0.8f,0.75f,1);
             }else if(player.direction.x > 0){ //east
                 player.currDirection = "NORTHEAST";
                 
-                transform.localPosition = new Vector3(0.066f,0.206f,0.5f);
+                transform.localPosition = new Vector3(0.066f,0.133f,0.5f);
                 
                 transform.localRotation = Quaternion.Euler(0,0,-10.16f);
                 
@@ -30,7 +30,7 @@ public class weaponScript : MonoBehaviour
             }else {//north
                 player.currDirection = "NORTH";
                 
-                transform.localPosition = new Vector3(0.0526000001f,0.211999997f,0.5f);
+                transform.localPosition = new Vector3(0.0526000001f,0.133f,0.5f);
                 
                 transform.localRotation = Quaternion.Euler(0,0,9.56465244f);
                 
@@ -41,7 +41,7 @@ public class weaponScript : MonoBehaviour
             if(player.direction.x < 0){ //west
                 player.currDirection = "SOUTHWEST";
                 
-                transform.localPosition = new Vector3(-0.0790000036f,0.189999998f,0.5f);
+                transform.localPosition = new Vector3(-0.0790000036f,0.133f,0.5f);
                 
                 transform.localRotation = Quaternion.Euler(0,0,19.511898f);
                 
@@ -49,7 +49,7 @@ public class weaponScript : MonoBehaviour
             }else if (player.direction.x > 0){ //east;
                 player.currDirection = "SOUTHEAST";
                 
-                transform.localPosition = new Vector3(-0.00200000009f,0.203999996f,-0.5f);
+                transform.localPosition = new Vector3(-0.00200000009f,0.133f,-0.5f);
                 
                 transform.localRotation = Quaternion.Euler(0,0,-19.5f);
                 
@@ -57,7 +57,7 @@ public class weaponScript : MonoBehaviour
             }else { //south
                 player.currDirection = "SOUTH";
                 
-                transform.localPosition = new Vector3(-0.0480000004f,0.204999998f,-0.5f);
+                transform.localPosition = new Vector3(-0.0480000004f, 0.133f,-0.5f);
                 
                 transform.localRotation = Quaternion.Euler(0,0,352.273254f);
                 
@@ -67,14 +67,14 @@ public class weaponScript : MonoBehaviour
             if(player.direction.x < 0){ //west
                 player.currDirection = "WEST";
                 
-                transform.localPosition = new Vector3(-0.0480000004f,0.202999994f,0.5f);
+                transform.localPosition = new Vector3(-0.0480000004f,0.133f,0.5f);
                 
                 transform.localRotation = Quaternion.Euler(0,0,28.6478519f);
                 
                 transform.localScale = new Vector3(1,.75f,1);
             }else if(player.direction.x > 0){  //east
                 player.currDirection  = "EAST";
-                transform.localPosition = new Vector3(0.0480000004f,0.202999994f,-0.5f);;
+                transform.localPosition = new Vector3(0.0480000004f,0.133f,-0.5f);;
                 transform.localRotation = Quaternion.Euler(0,0,-24.619f);
                 transform.localScale = new Vector3(1,.75f,1);
             }
