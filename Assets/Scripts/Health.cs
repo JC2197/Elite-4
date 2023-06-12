eb4e09ec3cc6c391ce354a3afd7fc5cbe6f2c67c
JC2197 <JoeConti21@gmail.com>
2023-06-11 21:59:21 -0400

    Squashed commit of the following:
    commit cbc810197c6da8474bd98d5b17ace96644aed9e6
    Author: JC2197 <JoeConti21@gmail.com>
    Date:   Sun Jun 11 21:53:59 2023 -0400
    
        Cleaned Some scripts, player animations and anima
    
    commit c77b3b19760bc079ceb9ead188ef0e84ca97096f
    Author: JC2197 <JoeConti21@gmail.com>
    Date:   Sun Jun 11 15:31:53 2023 -0400
    
        okok
    
    commit 5dfdf95f66cbf2a4fd0ed6108263c18c3c37050e
    Author: JC2197 <JoeConti21@gmail.com>
    Date:   Sun Jun 11 15:18:12 2023 -0400
    
        delete character movement
    
    commit e6a9a75a7b402280ceb02a4be52c865d0a2befd2
    Author: JC2197 <JoeConti21@gmail.com>
    Date:   Sun Jun 11 14:54:12 2023 -0400
    
        ok
    
        ok

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public HealthBar healthbar;
+    public int maxHealth = 100;
+    public int currentHealth; 
+    public bool isDead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+        void TakeDamage(int damage){
+        currentHealth -=damage;
+        healthbar.SetHealth(currentHealth);
+        
+        if(currentHealth==0){
+            isDead = true;
+        }
+    }
+}
